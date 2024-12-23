b4795efb2b47d6782eca061dbaac5dcae22162f9
Sam Bae <me@sambae.dev>
2024-12-23 16:39:36 +0000

    Fix function call

--- boards/shields/nice_view_gem/widgets/screen_peripheral.c
+++ boards/shields/nice_view_gem/widgets/screen_peripheral.c
@@ -113,7 +113,7 @@ int zmk_widget_screen_init(struct zmk_widget_screen *widget, lv_obj_t *parent) {
     lv_canvas_set_buffer(top, widget->cbuf, BUFFER_SIZE, BUFFER_SIZE, LV_IMG_CF_TRUE_COLOR);
 
     lv_obj_t *art = lv_img_create(widget->obj);
-    lv_img_set_src(art, GZyr5ohb0AAjzBg);
+    lv_img_set_src(art, &GZyr5ohb0AAjzBg);
     lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
 
     sys_slist_append(&widgets, &widget->node);
