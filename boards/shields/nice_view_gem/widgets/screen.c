3bde1c5c39eebfaa7a2378a5e9b902f3838093d3
Sam Bae <me@sambae.dev>
2024-12-26 20:05:56 +0000

    fix iby maybe

--- boards/shields/nice_view_gem/widgets/screen.c
+++ boards/shields/nice_view_gem/widgets/screen.c
@@ -186,7 +186,7 @@ int zmk_widget_screen_init(struct zmk_widget_screen *widget, lv_obj_t *parent) {
     //lv_obj_align(middle, LV_ALIGN_TOP_RIGHT, BUFFER_OFFSET_MIDDLE, 0);
     //lv_canvas_set_buffer(middle, widget->cbuf2, BUFFER_SIZE, BUFFER_SIZE, LV_IMG_CF_TRUE_COLOR);
     lv_obj_t *art = lv_img_create(widget->obj);
-    lv_obj_align(art, LV_ALIGN_TOP_LEFT, BUFFER_OFFSET_MIDDLE, 0);
+    lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
     lv_img_set_src(art, &iby);
 
 
