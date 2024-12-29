77c0d228b774f1f0d6f56d6d16c31817aed31364
Sam Bae <me@sambae.dev>
2024-12-29 04:31:05 +0000

    try some stuff

--- boards/shields/nice_view_gem/widgets/animation.c
+++ boards/shields/nice_view_gem/widgets/animation.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <zephyr/kernel.h>
-#include <zephyr/random/random.h>
 #include "animation.h"
 
 enum ART {
@@ -41,10 +40,8 @@ const lv_img_dsc_t *anim_hornet_imgs[] = {
 
 void draw_animation(lv_obj_t *canvas) {
 #if IS_ENABLED(CONFIG_NICE_VIEW_GEM_ANIMATION)
-    lv_obj_t *art = lv_animimg_create(canvas);
-    lv_obj_center(art);
-    
-    enum ART asset = (enum ART)(sys_rand32_get() % 3);
+    srand(k_uptime_get_32());
+    enum ART asset = rand() % 3;
 
     switch (asset) {
         case REINA:
@@ -52,12 +49,16 @@ void draw_animation(lv_obj_t *canvas) {
             lv_img_set_src(art, &reinamomo);
         break;
         case KNIGHT:
+            lv_obj_t *art = lv_animimg_create(canvas);
+            lv_obj_center(art);
             lv_animimg_set_src(art, (const void **)anim_knight_imgs, 8);
             lv_animimg_set_duration(art, CONFIG_NICE_VIEW_GEM_ANIMATION_MS);
             lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
             lv_animimg_start(art);
         break;
         case HORNET:
+            lv_obj_t *art = lv_animimg_create(canvas);
+            lv_obj_center(art);
             lv_animimg_set_src(art, (const void **)anim_hornet_imgs, 8);
             lv_animimg_set_duration(art, CONFIG_NICE_VIEW_GEM_ANIMATION_MS);
             lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
@@ -67,11 +68,11 @@ void draw_animation(lv_obj_t *canvas) {
 #else
     lv_obj_t *art = lv_img_create(canvas);
 
-    int length = sizeof(anim_imgs) / sizeof(anim_knight_imgs[0]);
+    int length = sizeof(anim_knight_imgs) / sizeof(anim_knight_imgs[0]);
     srand(k_uptime_get_32());
     int random_index = rand() % length;
 
-    lv_img_set_src(art, anim_imgs[random_index]);
+    lv_img_set_src(art, anim_knight_imgs[random_index]);
 #endif
 
     lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
